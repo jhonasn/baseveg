d1381b6df86e4c9d8eed6655ef89749be91d2952
jhonasn <jhonasn@gmail.com>
2020-04-03 01:20:20 -0300

    no favorites message implemented

--- app/src/favorites/index.js
+++ app/src/favorites/index.js
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from 'react'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import Loading from '../components/loading'
 import Banner from '../components/banner'
 import CardItem from '../list/card-item'
 import CardIngredient from '../ingredients/card-ingredient'
 import api from '../api/favorite'
 
+const useStyles = makeStyles(theme => ({
+  noFavorites: {
+    padding: theme.spacing(2),
+  },
+}))
+
 export default () => {
   // TODO: add category in items cards and item and category in options cards
   // TODO: add link to item in options cards
-  // TODO: add no favorites message
-  const [favorites, setFavorites] = useState([])
+  const theme = useTheme()
+  const classes = useStyles(theme)
+
+  const [favorites, setFavorites] = useState(null)
 
   const refreshFavorites = () => {
     (async () => setFavorites(await api.load()))()
@@ -18,10 +30,16 @@ export default () => {
 
   useEffect(refreshFavorites, [])
 
+  if (!favorites) return <Loading />
+
   return (
     <>
       <Banner
         title="Favoritos"
+        subtitle={
+          !!favorites && !!favorites.length &&
+          `${favorites.length} itens favoritos`
+        }
         imgName="favorites"
         isFullWidth
       />
@@ -52,6 +70,13 @@ export default () => {
                 />
               )
           ))}
+          {!favorites.length &&
+            <Paper className={classes.noFavorites}>
+              <Typography variant="body1">
+                Você não adicionou nenhum item aos seus favoritos
+              </Typography>
+            </Paper>
+          }
         </Grid>
       </Container>
     </>
